12d3dc04123ff513f886f2925a92f626576033fc
TheoOui <theoaz25@gmail.com>
2023-11-16 23:13:18 +0100

    J'arret la je vais craquer

--- client/src/components/LivretDeFamille/CreateLivretFamille.jsx
+++ client/src/components/LivretDeFamille/CreateLivretFamille.jsx
@@ -1,96 +1,89 @@
+// Importez les bibliothèques nécessaires
 import React, { useState, useEffect } from 'react';
-import Web3 from 'web3'; // Importez la bibliothèque Web3.js
-import { EthContext, useEth } from '../contexts/EthContext';
+import { EthContext, useEth } from './../../contexts/EthContext';
+import LivretDeFamilleContract from '../../contracts/LivretDeFamille.json';
+import LivretFamilleDetails from './LivretFamilleDetails';
 
-import LivretDeFamilleContract from '../contracts/LivretDeFamille.json';
+function CreateLivretFamille() {
+  const [parent1, setParent1] = useState('');
+  const [parent2, setParent2] = useState('');
+  const [contractData, setContractData] = useState({});
+  const { state } = useEth(EthContext);
 
+  // Chargez les données du contrat lorsque le composant est monté
+  const loadContractData = async () => {
+    try {
+      const nomParent1 = await state.contract.methods.nomParent1().call();
+      const nomParent2 = await state.contract.methods.nomParent2().call();
+      const nombreEnfants = await state.contract.methods.nombreEnfants().call();
+      const owner = await state.contract.methods.owner().call();
 
-require("dotenv").config();
+      setContractData({
+        parent1: nomParent1,
+        parent2: nomParent2,
+        nombreEnfants,
+        owner,
+      });
+    } catch (error) {
+      console.error('Erreur lors du chargement des données du contrat :', error);
+    }
+  };
 
+  useEffect(() => {
+    loadContractData();
+  }, []); // Assurez-vous que cette dépendance est correcte
 
+  const handleCreateLivret = async () => {
+    try {
+      if (window.ethereum) {
+        await window.ethereum.enable();
 
-  function CreateLivretFamille() {
-    const [parent1, setParent1] = useState('');
-    const [parent2, setParent2] = useState('');
-    const [contractData, setContractData] = useState({}); // Pour stocker les données du contrat
-    const {state} = useEth(EthContext);
+        const transaction = await state.contract.methods.createLivret(parent1, parent2).send({ from: state.accounts[0] });
 
-  
-    useEffect(() => {
-      // Chargez les données du contrat lorsque le composant est monté
-      const loadContractData = async () => {
-        try {
-          const nomParent1 = await contract.methods.nomParent1().call();
-          const nomParent2 = await contract.methods.nomParent2().call();
-          const nombreEnfants = await contract.methods.nombreEnfants().call();
-          const owner = await contract.methods.owner().call();
-  
-          setContractData({
-            nomParent1,
-            nomParent2,
-            nombreEnfants,
-            owner,
-          });
-        } catch (error) {
-          console.error('Erreur lors du chargement des données du contrat :', error);
-        }
-      };
-  
-      loadContractData();
-    }, []);
-  
-    const handleCreateLivret = async () => {
-      try {
-        if (window.ethereum) {
-          await window.ethereum.enable();
-          const accounts = await Web3.eth.getAccounts();
-  
-          const transaction = await contract.methods.createLivret(parent1, parent2).send({ from: accounts[0] });
-  
-          console.log('Transaction hash:', transaction.transactionHash);
-  
-          // Mettre à jour les données du contrat après la création du livret
-          const nomParent1 = await contract.methods.nomParent1().call();
-          const nomParent2 = await contract.methods.nomParent2().call();
-          const nombreEnfants = await contract.methods.nombreEnfants().call();
-          const owner = await contract.methods.owner().call();
-  
-          setContractData({
-            nomParent1,
-            nomParent2,
-            nombreEnfants,
-            owner,
-          });
-        } else {
-          console.error('Le navigateur ne prend pas en charge Ethereum');
-        }
-      } catch (error) {
-        console.error('Erreur lors de la création du livret de famille :', error);
+        console.log('Transaction hash:', transaction.transactionHash);
+        console.log('Prénoms lors de la création du livret :', parent1, parent2);
+        // Mettre à jour les données du contrat après la création du livret
+        await loadContractData();
+      } else {
+        console.error('Le navigateur ne prend pas en charge Ethereum');
       }
-    };
-  
-    return (
-      <div>
-        <h2>Créer un Livret de Famille</h2>
-        <input
-          type="text"
-          placeholder="Nom de Parent1"
-          value={parent1}
-          onChange={(e) => setParent1(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Nom de Parent2"
-          value={parent2}
-          onChange={(e) => setParent2(e.target.value)}
-        />
-        <button onClick={handleCreateLivret}>Créer Livret</button>
-  
-        {/* Bouton pour afficher les données du contrat */}
-        <button onClick={() => console.log(contractData)}>Afficher les données du contrat</button>
-      </div>
-    );
-  }
-  
-  export default CreateLivretFamille;
-  
+    } catch (error) {
+      console.error('Erreur lors de la création du livret de famille :', error);
+    }
+    
+  };
+  const handleDisplayContractData = async () => {
+    console.log("test") ;
+    console.log('Prénoms lors de la création du livret :', parent1, parent2);
+
+    // Appelez simplement la fonction loadContractData pour mettre à jour les détails du contrat
+    loadContractData(); }
+    
+    
+
+  return (
+    <div>
+      <h2>Créer un Livret de Famille</h2>
+      <input
+        type="text"
+        placeholder="Nom du Parent1"
+        value={parent1}
+        onChange={(e) => setParent1(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Nom de Parent2"
+        value={parent2}
+        onChange={(e) => setParent2(e.target.value)}
+      />
+      <button onClick={handleCreateLivret}>Créer Livret</button>
+
+
+
+
+      
+    </div>
+  );
+}
+
+export default CreateLivretFamille;
--- client/src/components/LivretDeFamille/LivretFamilleDetails.jsx
+++ client/src/components/LivretDeFamille/LivretFamilleDetails.jsx
@@ -1,22 +1,14 @@
+// Importez les bibliothèques nécessaires
+import React from 'react';
+import CreateLivretFamille from './CreateLivretFamille';
 
-import React, { useState, useEffect } from 'react';
-
-function LivretFamilleDetails() {
-  const [parent1, setParent1] = useState('');
-  const [parent2, setParent2] = useState('');
-  const [nombreEnfants, setNombreEnfants] = useState(0);
-
-  useEffect(() => {
-    // Récupérez les détails du livret de famille depuis le contrat intelligent
-    // Utilisez Web3.js ou ethers.js pour interagir avec le contrat intelligent ici
-  }, []);
-
+function LivretFamilleDetails({ parent1, parent2 }) {
   return (
     <div>
       <h2>Détails du Livret de Famille</h2>
-      <p>Parent1: {parent1}</p>
-      <p>Parent2: {parent2}</p>
-      <p>Nombre d'Enfants: {nombreEnfants}</p>
+      <p>Parent 1: {parent1}</p>
+      <p>Parent 2: {parent2}</p>
+      {/* ... (ajoutez d'autres détails du livret de famille si nécessaire) */}
     </div>
   );
 }
